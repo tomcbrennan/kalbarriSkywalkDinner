97ecd39a07b094a0b7c1cb78bd51f3d8c9936c58
tomcbrennan <tom.brennan22@gmail.com>
2021-04-26 16:31:26 +0800

    Design update + new details

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-// NAV HIDE ---------------------------------------------------
+// // // NAV CHANGE ---------------------------------------------------
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".nav").style.top = "0";
-  } else {
-    document.querySelector(".nav").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+// // const nav = document.querySelector('.nav');
+
+// // window.onscroll = function() {
+// //   var currentScrollPos = window.pageYOffset;
+// //   if (currentScrollPos > 0) {
+// //     nav.style.opacity="0"
+// //   } else {
+// //     nav.style.opacity="1";
+// //   }
+// // };
+
+
+
+// const hamburger = document.querySelector('.hamburger');
+// const mobileMenu = document.querySelector('.mobile-menu');
+// const navLink1 = document.querySelector('.mobile-link1');
+// const navLink2 = document.querySelector('.mobile-link2');
+// const navLink3 = document.querySelector('.mobile-link3');
+// const navLink4 = document.querySelector('.mobile-link4');
+
+// hamburger.addEventListener('click', () => {
+//   mobileMenu.classList.toggle('is-active');
+//   hamburger.classList.toggle('is-active');
+//   html.classList.toggle('is-active');
+// })
+
+// navLink1.addEventListener('click', () => {
+//   mobileMenu.classList.toggle('is-active');
+//   hamburger.classList.toggle('is-active');
+// })
+// navLink2.addEventListener('click', () => {
+//   mobileMenu.classList.toggle('is-active');
+//   hamburger.classList.toggle('is-active');
+// })
+// navLink3.addEventListener('click', () => {
+//   mobileMenu.classList.toggle('is-active');
+//   hamburger.classList.toggle('is-active');
+// })
+// navLink4.addEventListener('click', () => {
+//   mobileMenu.classList.toggle('is-active');
+//   hamburger.classList.toggle('is-active');
+// })
